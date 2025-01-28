f3a3765f74d896ecb492e8c5f25c7abb291f98b5
Prateek Gupta <prateek310303@gmail.com>
2025-01-28 18:14:52 +0530

    final
    commit

---
+++ clinet/src/components/Layout/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <></>
+    // <div className=" bg-dark text-light p-4">
+    //   <h6 className="text-center text-white">
+    //    Made BY Prateek Gupta
+    //   </h6>
+    // </div>
+  );
+};
+
+export default Footer;

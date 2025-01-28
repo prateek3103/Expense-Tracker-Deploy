20d1724ff56576cdb9cf514a1a121e488fadd306
Prateek Gupta <114001327+prateek3103@users.noreply.github.com>
2025-01-28 18:51:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,11 +26,11 @@ app.use("/api/v1/users", require("./routes/userRoute"));
 app.use("/api/v1/transections", require("./routes/transectionRoutes"));
 
 //static files
-// app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
-// app.get("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 //port
 const PORT = 8080 || process.env.PORT;
